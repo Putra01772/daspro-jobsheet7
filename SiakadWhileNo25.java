4c12ef713d4c0068cee8f55edd594d03879204f6
kawildan <kawildan01@gmail.com>
2025-11-07 16:46:20 +0700

    ModifikasiWhileNo25

--- SiakadWhileNo25.java
+++ SiakadWhileNo25.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-    public class SiakadWhileNo25 {
+public class SiakadWhileNo25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nilai, jml, i = 0;
@@ -17,24 +17,26 @@ import java.util.Scanner;
                 continue;
             }
 
-            if (nilai > 80 && nilai <= 100)
+            if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
-            else if (nilai > 73 && nilai <= 80)
+                System.out.println("Bagus, pertahankan nilainya!");
+            } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
-            else if (nilai > 65 && nilai <= 73)
+            } else if (nilai > 65 && nilai <= 73) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
-            else if (nilai > 60 && nilai <= 65)
+            } else if (nilai > 60 && nilai <= 65) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
-            else if (nilai > 50 && nilai <= 60)
+            } else if (nilai > 50 && nilai <= 60) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
-            else if (nilai > 39 && nilai <= 50)
+            } else if (nilai > 39 && nilai <= 50) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
-            else
+            } else {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+            }
 
             i++;
         }
+
+        sc.close();
     }
 }
-
-    
